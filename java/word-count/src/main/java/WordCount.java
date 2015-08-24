73a4b078a88c2798e50d94d5f9c6d8915086d590
gregorykrings <gregory.krings@gmail.com>
2015-08-24 18:03:35 +0200

    Update

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -4,18 +4,29 @@ import java.util.Map;
 public class WordCount {
     public Map<String, Integer> Phrase(String s)
     {
-        s = s.split("[^a-zA-Z0-9']+")[0];
+        System.out.println("s: "+s);
         String[] sArray = s.split(" ");
         Map<String, Integer> myMap = new HashMap<>();
         for(String sIt : sArray){
+            if(sIt.contains("[^a-zA-Z0-9']+")){
+                String[] minArr = sIt.split("[^a-zA-Z0-9']+");
+                if(minArr[0].contains("[^a-zA-Z0-9']+")){
+                    sIt = minArr[1];
+                }else{
+                    sIt = minArr[0];
+
+                }
+            }
             if(myMap.containsKey(sIt)){
-                int uVal = myMap.get(sIt.toLowerCase()).intValue()+1;
-                System.out.println("uVal: "+myMap.get(sIt.toLowerCase()).intValue());
+                int uVal = myMap.get(sIt.toLowerCase());
+                System.out.println("uVal: "+uVal);
                 myMap.put(sIt.toLowerCase(), uVal);
-            }else{
+            } else {
                 myMap.put(sIt.toLowerCase(),1);
             }
+            System.out.println("sIt: "+sIt);
         }
+        System.out.println("myMap: "+myMap.toString());
         return myMap;
     }
 }
